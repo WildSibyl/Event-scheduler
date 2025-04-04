85273c03b5d12156cd526e7429f277de07ab2fd9
Giada <giada.bellan@live.it>
2025-04-04 15:01:19 +0200

    responsiveness update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -51,19 +51,19 @@ export const Home = () => {
   return (
     <div className="min-h-screen flex flex-col bg-gradient-to-b from-blue-400 to-blue-200">
       {/* Header */}
-      <header className="text-white bg-blue-400 py-4 text-2xl font-bold shadow-md flex justify-between items-center px-4 md:flex-row sm:flex-col">
+      <header className="text-white bg-blue-400 py-4 text-2xl font-bold shadow-md flex justify-between items-center px-4 md:flex-row flex-col ">
         <span>My Event App</span>
         {user ? (
           <>
             <div className="flex space-x-3">
               <button
-                className="bg-white text-gray-800 rounded-full shadow-md px-8 py-2 flex items-center cursor-pointer hover:bg-blue-200 hover:shadow-lg transition duration-300 ease-in-out md:mt-0 sm:mt-4"
+                className="bg-white text-gray-800 rounded-full shadow-md px-8 py-2 flex items-center cursor-pointer hover:bg-blue-200 hover:shadow-lg transition duration-300 ease-in-out md:mt-0 mt-4"
                 onClick={() => navigate("/new-event")}
               >
                 <span className="font-bold text-xxl">Create Event</span>
               </button>
               <button
-                className="bg-white text-gray-800 rounded-full shadow-md px-8 py-2 cursor-pointer hover:bg-red-200 transition duration-300"
+                className="bg-white text-gray-800 rounded-full shadow-md px-8 py-2 cursor-pointer hover:bg-red-200 transition duration-300 ease-in-out md:mt-0 mt-4"
                 onClick={logout}
               >
                 Logout
@@ -75,13 +75,13 @@ export const Home = () => {
             <div className="flex space-x-3">
               <button
                 onClick={() => navigate("/login")}
-                className="bg-white text-gray-800 rounded-full shadow-md px-8 py-2 flex items-center cursor-pointer hover:bg-blue-200 hover:shadow-lg transition duration-300 ease-in-out md:mt-0 sm:mt-4"
+                className="bg-white text-gray-800 rounded-full shadow-md px-8 py-2 flex items-center cursor-pointer hover:bg-blue-200 hover:shadow-lg transition duration-300 ease-in-out md:mt-0 mt-4"
               >
                 Login
               </button>
               <button
                 onClick={() => navigate("/signup")}
-                className="bg-white text-gray-800 rounded-full shadow-md px-8 py-2 flex items-center cursor-pointer hover:bg-blue-200 hover:shadow-lg transition duration-300 ease-in-out md:mt-0 sm:mt-4"
+                className="bg-white text-gray-800 rounded-full shadow-md px-8 py-2 flex items-center cursor-pointer hover:bg-blue-200 hover:shadow-lg transition duration-300 ease-in-out md:mt-0 mt-4"
               >
                 Signup
               </button>
